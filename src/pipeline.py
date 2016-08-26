f235d4108f1af67c318e70d0a7aba64dca50a6a9
raphaelcampos <raphaelrcampos@yahoo.com.br>
2016-08-26 11:46:20 -0300

    predict in batch

--- src/pipeline.py
+++ src/pipeline.py
@@ -9,10 +9,16 @@ from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import confusion_matrix
 
 import pandas as pd
+import numpy as np
 
 import preprocessing as pre
 
+import xgboost as xgb
+
+
 def score_mmc(estimator, X, y):
+	print(estimator.predict(X))
+	print(y)
 	return matthews_corrcoef(estimator.predict(X), y)
 
 seed = 1234
@@ -34,9 +40,13 @@ X_train, y_train = X_train[idx], y_train[idx]
 
 # Defining pipeline and params
 rf = RandomForestClassifier(n_estimators=100, n_jobs=-1, class_weight="balanced_subsample", random_state=seed)
+
+xboost = xgb.XGBClassifier()
+
 pipeline = Pipeline(steps=[('rf', rf)])
 params = {
-	# "lr__C" = []
+	#"rf__max_features" : ['log2', 'sqrt', 0.08, 0.15, 0.3, 0.7, 1.0]
+	#"boost__learning_rate" : [0.1, 0.3, 0.7, 1.0]
 }
 cv = GridSearchCV(pipeline, params, scoring=score_mmc, verbose=5)
 
@@ -45,9 +55,9 @@ cv.fit(X_train, y_train)
 best_model = cv.best_estimator_
 
 # saving memory again... 
-X_train = pre.load_dataset("../data/test_numeric.csv", batch = 100000, no_label = True)
+#X_train = pre.load_dataset("../data/test_numeric.csv", batch = 100000, no_label = True)
 
 # Predicting test data and saving it for submission
 df = pd.read_csv("../data/sample_submission.csv")
-df['Response'] = best_model.predict(X_train)
+df['Response'] = pre.predict_batch(best_model, "../data/test_numeric.csv", 200000)
 df.to_csv("../data/submission_%s.csv" % pipeline.steps[0][0], index=False)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -89,3 +89,74 @@ def load_dataset(file_name, batch = 1000, no_label = False):
 
 	return matrix, np.asarray(label)
 
+def predict_batch(estimator, file_name, batch=10000):
+	file = open(file_name)
+
+	csvreader = csv.reader(file)
+	
+	# ignore headers
+	next(csvreader, None)
+	
+	rows = []
+	columns = []
+	data = []
+	row = 0
+
+	srow = np.ndarray(0)
+	scol = np.ndarray(0)
+	sdata = np.ndarray(0)
+
+	matrix = None
+	label = []
+	i = 0
+	pred = np.ndarray(0)
+	for line in csvreader:
+		d = np.array(line, dtype=str)
+		
+		c = np.where(d != '')[0]
+		
+		dim = d.shape[0] - 1
+		rows.append(np.repeat(row, c.shape[0] - 1))
+		columns.append(c[1:] - 1)
+		data.append(d[c[1:]].astype(float))
+
+		row = row + 1
+
+		if(row%batch == 0):
+			rows = np.concatenate(rows)
+			
+			#columns.append(scol)
+			columns = np.concatenate(columns)
+			
+			#data.append(sdata)
+			data = np.concatenate(data)
+
+
+			matrix = sparse.csr_matrix((data, (rows, columns)), (batch, dim))
+			
+			# cleaning data 
+			rows = []
+			columns = []
+			data = []
+
+			pred = np.concatenate((pred, estimator.predict(matrix)))
+			
+			print(i)
+			row = 0
+			i = i + 1
+
+	if(not row == 0):
+		rows = np.concatenate(rows)
+		columns = np.concatenate(columns)
+		data = np.concatenate(data)
+
+		matrix = sparse.csr_matrix((data, (rows, columns)), (row, dim))
+
+		# cleaning data 
+		rows = []
+		columns = []
+		data = []
+
+		pred = np.concatenate((pred, estimator.predict(matrix)))
+
+	return pred
