1617f90c18f1b8087c64ef1c98d6e55613339c18
Gustavo Penha <guzpenha@Guz-notebook>
2016-08-27 12:47:53 -0300

    Adding option parser.
    ex: running pipeline.py and make predictions for test file:
    	python pipeline.py -p True

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,7 @@
-from sklearn.datasets import load_svmlight_file
 from IPython import embed
+from optparse import OptionParser
+
+from sklearn.datasets import load_svmlight_file
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.grid_search import GridSearchCV
@@ -21,43 +23,45 @@ def score_mmc(estimator, X, y):
 	print(y)
 	return matthews_corrcoef(estimator.predict(X), y)
 
-seed = 1234
-np.random.seed(seed)
+if __name__ == "__main__":
 
-# Loading datasets with i = 100000
-X_train, y_train = pre.load_dataset("../data/train_numeric.csv", batch = 100000)
+	parser = OptionParser()
+	parser.add_option("-p","--make_predictions",help="make predictions to file.",default = False)
+	args = parser.parse_args()[0]
 
-y0_idx = np.where(y_train == 0)[0]
-y1_idx = np.where(y_train == 1)[0]
-np.random.shuffle(y0_idx)
+	seed = 1234
+	np.random.seed(seed)
 
-idx = np.concatenate((y1_idx, y0_idx[:(10*y1_idx.shape[0])]))
+	# Loading datasets with i = 100000
+	X_train, y_train = pre.load_dataset("../data/train_numeric.csv", batch = 100000)
 
-# I'm doing this in order to save memory (the ideal it is not that hehe)
-X_train, y_train = X_train[idx], y_train[idx]
+	y0_idx = np.where(y_train == 0)[0]
+	y1_idx = np.where(y_train == 1)[0]
+	np.random.shuffle(y0_idx)
 
-#X_test,y_test = load_svmlight_file("../data/test_numeric.svm")
+	idx = np.concatenate((y1_idx, y0_idx[:(10*y1_idx.shape[0])]))
 
-# Defining pipeline and params
-rf = RandomForestClassifier(n_estimators=100, n_jobs=-1, class_weight="balanced_subsample", random_state=seed)
+	# I'm doing this in order to save memory (the ideal it is not that hehe)
+	X_train, y_train = X_train[idx], y_train[idx]
 
-xboost = xgb.XGBClassifier()
+	# Defining pipeline and params
+	rf = RandomForestClassifier(n_estimators=100, n_jobs=-1, class_weight="balanced_subsample", random_state=seed)
 
-pipeline = Pipeline(steps=[('rf', rf)])
-params = {
-	#"rf__max_features" : ['log2', 'sqrt', 0.08, 0.15, 0.3, 0.7, 1.0]
-	#"boost__learning_rate" : [0.1, 0.3, 0.7, 1.0]
-}
-cv = GridSearchCV(pipeline, params, scoring=score_mmc, verbose=5)
+	xboost = xgb.XGBClassifier()
 
-# Fitting  CV
-cv.fit(X_train, y_train)
-best_model = cv.best_estimator_
+	pipeline = Pipeline(steps=[('rf', rf)])
+	params = {
+		#"rf__max_features" : ['log2', 'sqrt', 0.08, 0.15, 0.3, 0.7, 1.0]
+		#"boost__learning_rate" : [0.1, 0.3, 0.7, 1.0]
+	}
+	cv = GridSearchCV(pipeline, params, scoring=score_mmc, verbose=5)
 
-# saving memory again... 
-#X_train = pre.load_dataset("../data/test_numeric.csv", batch = 100000, no_label = True)
+	# Fitting  CV
+	cv.fit(X_train, y_train)
+	best_model = cv.best_estimator_
 
-# Predicting test data and saving it for submission
-df = pd.read_csv("../data/sample_submission.csv")
-df['Response'] = pre.predict_batch(best_model, "../data/test_numeric.csv", 200000)
-df.to_csv("../data/submission_%s.csv" % pipeline.steps[0][0], index=False)
+	# Predicting test data and saving it for submission
+	if(args.make_predictions):
+		df = pd.read_csv("../data/sample_submission.csv")
+		df['Response'] = pre.predict_batch(best_model, "../data/test_numeric.csv", 200000)
+		df.to_csv("../data/submission_%s.csv" % pipeline.steps[0][0], index=False)
