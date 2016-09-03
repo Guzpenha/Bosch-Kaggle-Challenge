72d7d078e63e360faabc96e12656d1a112dfb523
guzpenha <guzpenha10@gmail.com>
2016-09-03 18:29:11 -0300

    Minor modifications

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,4 +1,24 @@
-from IPython import embed
+from IPython import embedfrom optparse import OptionParser
+
+from sklearn.datasets import load_svmlight_file
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.grid_search import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import confusion_matrix
+from scipy.sparse import csr_matrix, hstack
+from scipy.io import mmread
+from sklearn.cross_validation import train_test_split
+
+import pandas as pd
+import numpy as np
+
+import preprocessing as pre
+
+import xgboost as xgb
+
 from optparse import OptionParser
 
 from sklearn.datasets import load_svmlight_file
@@ -10,7 +30,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import confusion_matrix
 from scipy.sparse import csr_matrix, hstack 
-
+from scipy.io import mmread
 from sklearn.cross_validation import train_test_split
 
 import pandas as pd
@@ -36,7 +56,12 @@ if __name__ == "__main__":
 
 	# Loading datasets with i = 100000
 	X_train, y_train = pre.load_dataset("../data/train_numeric.csv", batch = 100000)
-	# X_date = pre.load_date_features("../data/train_date.csv", batch = 100000)
+	
+	#X_date = pre.load_date_features("../data/train_date.csv", batch = 100000)	   
+	#X_categorical = mmread("../data/train_categorical.mtx")
+	
+	#X_train = hstack(X_train,X_date)
+	#X_train = hstack(X_train,X_categorical)
 
 	X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=seed)
 
@@ -47,21 +72,20 @@ if __name__ == "__main__":
 
 	# I'm doing this in order to save memory (the ideal it is not that hehe)
 	idx = np.concatenate((y1_idx, y0_idx[:(10*y1_idx.shape[0])]))
-
-	X_train, y_train = hstack(X_train[idx,],X_date[idx]), y_train[idx]
-	# X_train, y_train = X_date[idx],y_train[idx]
-	# X_train, y_train = hstack((X_train[idx],X_date[idx])),y_train[idx]
-
+	X_train, y_train = X_train[idx], y_train[idx]
+	
 	# Defining pipeline and params
 	rf = RandomForestClassifier(n_estimators=50, n_jobs=-1, random_state=42)
-	xboost = xgb.XGBClassifier(max_depth=100, n_estimators=200)
+	xgboost = xgb.XGBClassifier(max_depth=100, n_estimators=200)
 
-	pipeline = Pipeline(steps=[('rf', rf)])
+	pipeline = Pipeline(steps=[('xgb', xgboost)])
+	#pipeline = Pipeline(steps= [('rf',rf)])
+
 	params = {
-		"rf__max_features" : ['log2', 'sqrt', 0.08, 0.15, 0.3, 0.7, 1.0]
-		#"boost__learning_rate" : [0.1, 0.3, 0.7, 1.0]
+		#"rf__max_features" : ['log2', 'sqrt', 0.08, 0.15, 0.3, 0.7, 1.0]
+		#"xgb__learning_rate" : [0.1, 0.3, 0.7, 1.0]
 	}
-	cv = GridSearchCV(pipeline, params, scoring=score_mmc, verbose=5)
+	cv = GridSearchCV(pipeline, params, scoring=score_mmc, cv=5, verbose=5)
 
 	# Fitting  CV
 	cv.fit(X_train, y_train)
