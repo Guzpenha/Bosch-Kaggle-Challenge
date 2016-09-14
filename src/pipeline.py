4954acdb8fd4fb4fec831264b4d72c5d0de02286
Gustavo Penha <guzpenha10@gmail.com>
2016-09-13 23:19:55 -0300

    Modularizing and adding custom cv to code

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,34 +1,57 @@
 from IPython import embed
-from sklearn.cross_validation import train_test_split
 from optparse import OptionParser
+import os  
 
 from sklearn.datasets import load_svmlight_file
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.grid_search import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
-
 from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import confusion_matrix
-from scipy.sparse import csr_matrix, hstack
-from scipy.io import mmread
-from scipy.io import mmwrite
-
-import pandas as pd
-import numpy as np
-
 from sklearn.cross_validation import train_test_split
-
-import preprocessing as pre
-from  preprocessing import extract_missing
 
 import xgboost as xgb
 
-import csv
+from scipy.sparse import csr_matrix, hstack
+from scipy.io import mmread
+from scipy.io import mmwrite
 from scipy import sparse
 import scipy
 
+import pandas as pd
+import numpy as np
+import csv
+
+from  preprocessing import extract_missing
+import preprocessing as pre
 import validation as val
+
+
+def load_full_dataset():	
+	# If X_train.mtx have not been generated yet 
+	if( not os.path.exists('../data/X_train.mtx')):
+		X_train, y_train = pre.load_dataset("../data/train_numeric.csv", batch = 100000)
+		X_date = pre.load_date_features("../data/train_date.csv", batch = 100000)
+		# TO DO
+		# X_train_cat = 
+		mmwrite('../data/X_train',X_train)		
+		mmwrite('../data/train_date',X_date)
+		# mmwrite('../data/train_categorical',X_train_cat)
+
+	else:
+		X_train = mmread('../data/X_train')
+		X_train = X_train.tocsr()
+		y_train = pre.load_labels("../data/train_numeric.csv")
+		X_date = mmread('../data/train_date')
+		X_train_cat = mmread('../data/train_categorical')
+
+	csvreader = csv.reader(open("../data/train_numeric.csv"))
+	header = next(csvreader, None)
+	X_train = scipy.sparse.hstack((X_train, extract_missing(X_train, header[1:-1]), X_train_cat)).tocsr()
+	X_train = scipy.sparse.hstack((X_train,X_date))	
+
+	return X_train.tocsc(), y_train
 
 def score_mcc(estimator, X, y):
 	import sklearn as skl
@@ -42,71 +65,39 @@ if __name__ == "__main__":
 
 	seed = 42
 	np.random.seed(seed)
-	# Loading datasets with i = 100000
-	# X_train, y_train = pre.load_dataset("../data/train_numeric.csv", batch = 100000)
-	# X_date = pre.load_date_features("../data/train_date.csv", batch = 100000)
-	# mmwrite('../data/train_date',X_date)
-	# mmwrite('../data/X_train',X_train)	
-	X_train = mmread('../data/X_train')
-	X_train = X_train.tocsr()
-	y_train = pre.load_labels("../data/train_numeric.csv")
-	X_date = mmread('../data/train_date')
-	# X_date2 = pre.load_date_closest_labels_percentage("../data/train_date.csv","../data/train_numeric.csv")
-	X_train_cat = mmread('../data/train_categorical')
 
+	# Loading dataset with all features
+	print("Loading features")
+	X_train, y_train = load_full_dataset()		
 	embed()
 
-	csvreader = csv.reader(open("../data/train_numeric.csv"))
-	header = next(csvreader, None)
-
-	X_train = scipy.sparse.hstack((X_train, extract_missing(X_train, header[1:-1]), X_train_cat)).tocsr()
-	X_train = scipy.sparse.hstack((X_train,X_ate))
-	#X_train = scipy.sparse.hstack((X_train,X_date2))
-dx = np.where(y_train == 0)[0]
-	del X_train_cat
-	del X_date
-	#del X_date2
-
-	#X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=seed)
-
-	#y0_idx = np.where(y_train == 0)[0]
-	#y1_idx = np.where(y_train == 1)[0]
-	#np.random.shuffle(y0_idx)
-
-	#idx = np.concatenate((y1_idx, y0_idx[:(10*y1_idx.shape[0])]))
-
-	# I'm doing this in order to save memory (the ideal it is not that hehe)
-	#X_train, y_train = X_train[idx], y_train[idx]
-	
 	# Defining pipeline and params
-	rf = RandomForestClassifier(n_estimators=300, max_features=0.08, n_jobs=-1, random_state=seed)
-
-	xboost = xgb.XGBClassifier(n_estimators=200, seed=0)
-
+	xboost = xgb.XGBClassifier(seed=0)
+	# rf = RandomForestClassifier(n_estimators=300, max_features=0.08, n_jobs=-1, random_state=seed)
 	pipeline = Pipeline(steps=[('xboost', xboost)])
 	params = {
 		#"rf__max_features" : ['log2', 'sqrt', 0.08, 0.15, 0.3, 0.7, 1.0]
 		#"xboost__learning_rate" : [0.1, 0.3, 0.7, 1.0]
-		"xboost__max_depth": [5,8,10,12],
-		"xboost__min_child_weight": [1,3,6]
+		# "xboost__max_depth": [5,8,10,12],
+		# "xboost__min_child_weight": [1,3,6],
+		# "xboost__n_estimators": [100,150,200]
+		"xboost__max_depth": [12],
+		"xboost__min_child_weight": [3],
+		"xboost__n_estimators": [200]
 	}
 	
 	#Fitting custom CV with correct ratios
-	best_params = val.GridsearchBestRatio(X_train.tocsc(), y_train, pipeline, scoring=score_mcc, verbose=5, ratios=[0.05], params=params)
-	#cv = GridSearchCV(pipeline, params, scoring=score_mcc, verbose=5, cv = 5)
+	print("Running CV")
+	best_params = val.GridsearchBestRatio(X_train, y_train, pipeline, scoring=score_mcc, verbose=5, ratios=[0.05], params=params)	
 	
-	#cv.fit(X_train, y_train)
-	#best_model = cv.best_estimator_
-	#print(score_mcc(cv, X_test, y_test))
-	#best_model.fit(scipy.sparse.vstack((X_train, X_test)), np.concatenate((y_train, y_test)))
+	# best_model = xgb.XGBClassifier(n_estimators = 200, max_depth = best_params[1]["max_depth"],min_child_weight=best_params[1]["min_child_weight"])
+	# best_model.fit(X_train,y_train)			
 
 	# Predicting test data and saving it for submission
 	if(args.make_predictions):
 		X_board = pre.load_dataset("../data/test_numeric.csv", batch = 100000, no_label=True)
 		X_board_cat = mmread('../data/test_categorical')
-		X_board_date = pre.load_date_features("../data/test_date.csv", batch = 100000)
-
-		# X_date2 =
+		X_board_date = pre.load_date_features("../data/test_date.csv", batch = 100000)		
 		X_board = scipy.sparse.hstack((X_board, extract_missing(X_board, header[1:-1]), X_board_cat)).tocsr()
 		X_board = scipy.sparse.hstack((X_board,X_board_date))
 		
